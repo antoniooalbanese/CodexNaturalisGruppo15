13252ef850faa163ae4f3140a8f5bc675d0bb034
Flash1408 <114903849+Flash1408@users.noreply.github.com>
2024-05-02 12:39:00 +0200

    Test main

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,5 +1,10 @@
 package application;
 
+import com.google.gson.Gson;
+
 public class Application {
-	
+	public static void main(String[] args) {
+		Gson gson = new Gson();
+		System.out.println(gson.toJson("Hello World"));
+	}
 }
