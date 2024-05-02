de2c9d9852906577e0cf1054136875e5a88af20c
Flash1408 <114903849+Flash1408@users.noreply.github.com>
2024-05-02 14:55:47 +0200

    Correzione file pom

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,10 +1,25 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 import com.google.gson.Gson;
 
 public class Application {
 	public static void main(String[] args) {
-		Gson gson = new Gson();
-		System.out.println(gson.toJson("Hello World"));
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(Application.class.getClassLoader().getResourceAsStream("MazzoOro.json"), StandardCharsets.UTF_8))) {
+			String line;
+			StringBuilder builder = new StringBuilder();
+			
+			while ((line = reader.readLine()) != null) {
+				builder.append(line).append("\n");
+			}
+			
+			System.out.println(builder.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
