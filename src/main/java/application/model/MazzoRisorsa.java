189999ee9f2522ce76b8732d155cb0d55c921364
antoniooalbanese <a.albanese@studenti.unibg.it>
2024-05-03 11:10:03 +0200

    Aggiunta del metodo per caricare il mazzo risorsa

--- src/main/java/application/model/MazzoRisorsa.java
+++ src/main/java/application/model/MazzoRisorsa.java
@@ -1,6 +1,12 @@
 package application.model;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 /**
  * Questa classe rappresenta il mazzo delle carte risorsa.
@@ -12,12 +18,30 @@ public class MazzoRisorsa {
 	ArrayList<CartaRisorsa> mazzo = new ArrayList<CartaRisorsa>(40);
 	
 	/**
-	 * Costruttore della classe.
+	 * Costruttore della classe, inizializzando il mazzo vuoto.
 	 */
 	public MazzoRisorsa(ArrayList<CartaRisorsa> mazzo) {
-		this.mazzo = mazzo;
+		this.mazzo = null;
 	}
-	
+
+	/**
+	 * Metodo per caricare il mazzo delle carte risorsa partendo dal file json MazzoRisorsa.json.
+	 * @return
+	 */
+	public ArrayList<CartaRisorsa> loadMazzoR(){
+		Gson gson = new Gson();
+		String filename = "MazzoRisorsa.json";
+        ArrayList<CartaRisorsa> risorsa = new ArrayList<CartaRisorsa>();
+
+        try (FileReader reader = new FileReader(filename)){
+            Type listType = new TypeToken<ArrayList<CartaRisorsa>>(){}.getType();
+           risorsa = gson.fromJson(reader, listType);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return risorsa;
+	}
 	
 	/**
 	 * BISOGNA RICORDARE CHE IL MAZZO E' DA VISUALIZZARE MOSTRANDO IL RETRO
