eaa1fa6fe51f51f14f28091fa937efeeb42507a6
Flash1408 <114903849+Flash1408@users.noreply.github.com>
2024-05-03 10:59:23 +0200

    Aggiunta del metodo per caricare il mazzo oro

--- src/main/java/application/model/MazzoOro.java
+++ src/main/java/application/model/MazzoOro.java
@@ -1,6 +1,12 @@
 package application.model;
 
 import java.util.ArrayList;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+
 
 /**
  * Questa classe rappresenta il mazzo delle carte oro.
@@ -12,14 +18,33 @@ public class MazzoOro {
 	ArrayList<CartaOro> mazzo = new ArrayList<CartaOro>(40);
 	
 	/**
-	 * Costruttore della classe.
+	 * Costruttore della classe, inizializzando il mazzo vuoto.
 	 */
 	public MazzoOro(ArrayList<CartaOro> mazzo) {
-		this.mazzo = mazzo;
+		this.mazzo = null;
 	}
 	
+	/**
+	 * Metodo per caricare il mazzo delle carte oro partendo dal file json MazzoOro.json.
+	 * @return
+	 */
+	public ArrayList<CartaOro> loadMazzoO(){
+		Gson gson = new Gson();
+		String filename = "MazzoOro.json";
+        ArrayList<CartaOro> oro = new ArrayList<CartaOro>();
+
+        try (FileReader reader = new FileReader(filename)){
+            Type listType = new TypeToken<ArrayList<CartaRisorsa>>(){}.getType();
+            oro = gson.fromJson(reader, listType);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return oro;
+	}
+
+	}
 	/**
 	 * BISOGNA RICORDARE CHE IL MAZZO E' DA VISUALIZZARE MOSTRANDO IL RETRO
 	 * DELLE CARTE.
 	 */
-}
