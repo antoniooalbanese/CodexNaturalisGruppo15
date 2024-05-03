ac0031b7c9b762a86c9c90600969a3f20a99c6ef
antoniooalbanese <a.albanese@studenti.unibg.it>
2024-05-03 12:01:55 +0200

    Aggiunta del metodo per caricare il mazzo delle carte iniziali

--- src/main/java/application/model/MazzoIniziale.java
+++ src/main/java/application/model/MazzoIniziale.java
@@ -1,6 +1,12 @@
 package application.model;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 /**
  * Questa classe rappresenta il mazzo delle carte iniziali.
@@ -12,10 +18,29 @@ public class MazzoIniziale {
 	ArrayList<CartaIniziale> mazzo = new ArrayList<CartaIniziale>(6);
 	
 	/**
-	 * Costruttore della classe.
+	 * Costruttore della classe, inizializzando il mazzo vuoto.
 	 */
 	public MazzoIniziale(ArrayList<CartaIniziale> mazzo) {
-		this.mazzo = mazzo;
+		this.mazzo = null;
+	}
+	
+	/**
+	 * Metodo per caricare il mazzo delle carte iniziali partendo dal file json MazzoIniziale.json.
+	 * @return
+	 */
+	public ArrayList<CartaIniziale> loadMazzoI(){
+		Gson gson = new Gson();
+		String filename = "MazzoIniziale.json";
+        ArrayList<CartaIniziale> iniziale = new ArrayList<CartaIniziale>();
+
+        try (FileReader reader = new FileReader(filename)){
+            Type listType = new TypeToken<ArrayList<CartaIniziale>>(){}.getType();
+           iniziale = gson.fromJson(reader, listType);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return iniziale;
 	}
 	
 	/**
