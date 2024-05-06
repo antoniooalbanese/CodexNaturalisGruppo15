fd6badd575ab64c930d1b4f0b5f877879053a297
antoniooalbanese <a.albanese@studenti.unibg.it>
2024-05-06 19:14:50 +0200

    modifica commento

--- src/main/java/application/model/TipoAngolo.java
+++ src/main/java/application/model/TipoAngolo.java
@@ -1,7 +1,7 @@
 package application.model;
 
 /**
- * Questo enumerativo descrive il tipo di angolo presente in una carta del gioco.
+ * Questo enum descrive il tipo di angolo presente in una carta del gioco.
  */
 public enum TipoAngolo {
 	VUOTO, NASCOSTO, RISORSA, OGGETTO
